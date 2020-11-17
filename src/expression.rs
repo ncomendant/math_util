3f7ddc402e688cb32e5e221c4c6dded9cd2c7caf
Nicholas Comendant <nicholas.comendant@gmail.com>
2020-11-17 01:20:32 -0800

    refactor expression and follow operation priority

--- src/expression.rs
+++ src/expression.rs
@@ -1,106 +1,206 @@
 use crate::rational_number::RationalNumber;
-use crate::LibError;
+use crate::{LibError, evaluate_expression};
 
 #[derive(Debug, Clone)]
-struct Expression {
-    pub number: RationalNumber,
-    pub parent: Option<Operation>,
+pub enum ExpressionValue {
+    Expression(Expression),
+    Number(RationalNumber),
+}
+
+impl ExpressionValue {
+    pub fn expression(&self) -> &Expression {
+        match self {
+            ExpressionValue::Expression(e) => e,
+            _ => panic!("not a number")
+        }
+    }
+
+    pub fn number(&self) -> &RationalNumber {
+        match self {
+            ExpressionValue::Number(n) => n,
+            _ => panic!("not a number")
+        }
+    }
+}
+
+impl From<Expression> for ExpressionValue {
+    fn from(e: Expression) -> Self {
+        ExpressionValue::Expression(e)
+    }
+}
+
+impl From<RationalNumber> for ExpressionValue {
+    fn from(n: RationalNumber) -> Self {
+        ExpressionValue::Number(n)
+    }
+}
+
+impl From<u32> for ExpressionValue {
+    fn from(n: u32) -> Self {
+        ExpressionValue::Number(n.into())
+    }
+}
+
+impl From<i32> for ExpressionValue {
+    fn from(n: i32) -> Self {
+        ExpressionValue::Number(n.into())
+    }
+}
+
+impl From<f32> for ExpressionValue {
+    fn from(n: f32) -> Self {
+        ExpressionValue::Number(n.into())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Expression {
+    pub values: Vec<ExpressionValue>,
+    pub operations: Vec<Operation>,
 }
 
 impl Expression {
-    pub fn new<T: Into<RationalNumber>>(n: T) -> Self {
+    pub fn new<T: Into<ExpressionValue>>(n: T) -> Self {
         Expression {
-            number: n.into(),
-            parent: None,
+            values: vec![n.into()],
+            operations: Vec::new(),
         }
     }
 
-    pub fn pow<T: Into<RationalNumber>>(&mut self, n: T) -> Self {
-        Expression {
-            parent: Some(Operation::Exponent(Box::new(self.clone()))),
-            number: n.into(),
-        }
+    pub fn pow<T: Into<ExpressionValue>>(&self, n: T) -> Self {
+        let mut e = self.clone();
+        e.values.push(n.into());
+        e.operations.push(Operation::Exponent);
+        e
     }
 
-    pub fn divide<T: Into<RationalNumber>>(&mut self, n: T) -> Self {
-        Expression {
-            parent: Some(Operation::Division(Box::new(self.clone()))),
-            number: n.into(),
-        }
+    pub fn divide<T: Into<ExpressionValue>>(&self, n: T) -> Self {
+        let mut e = self.clone();
+        e.values.push(n.into());
+        e.operations.push(Operation::Division);
+        e
     }
 
-    pub fn multiply<T: Into<RationalNumber>>(&mut self, n: T) -> Self {
-        Expression {
-            parent: Some(Operation::Multiplication(Box::new(self.clone()))),
-            number: n.into(),
-        }
+    pub fn multiply<T: Into<ExpressionValue>>(&self, n: T) -> Self {
+        let mut e = self.clone();
+        e.values.push(n.into());
+        e.operations.push(Operation::Multiplication);
+        e
     }
 
-    pub fn add<T: Into<RationalNumber>>(&mut self, n: T) -> Self {
-        Expression {
-            parent: Some(Operation::Addition(Box::new(self.clone()))),
-            number: n.into(),
-        }
+    pub fn add<T: Into<ExpressionValue>>(&self, n: T) -> Self {
+        let mut e = self.clone();
+        e.values.push(n.into());
+        e.operations.push(Operation::Addition);
+        e
     }
 
-    pub fn subtract<T: Into<RationalNumber>>(&mut self, n: T) -> Self {
-        Expression {
-            parent: Some(Operation::Subtraction(Box::new(self.clone()))),
-            number: n.into(),
-        }
+    pub fn subtract<T: Into<ExpressionValue>>(&self, n: T) -> Self {
+        let mut e = self.clone();
+        e.values.push(n.into());
+        e.operations.push(Operation::Subtraction);
+        e
     }
 
     pub fn evaluate(&self) -> RationalNumber {
-        if let Some(p) = &self.parent {
-            match p {
-                Operation::Exponent(e) =>  e.evaluate().pow(&self.number),
-                Operation::Division(e) => e.evaluate() / self.number,
-                Operation::Multiplication(e) => e.evaluate() * self.number,
-                Operation::Addition(e) => e.evaluate() + self.number,
-                Operation::Subtraction(e) => e.evaluate() - self.number,
+        let mut expr = self.clone();
+        loop {
+            if let Some(val) = expr.evaluate_next() {
+                match val {
+                    ExpressionValue::Expression(e) => expr = e,
+                    ExpressionValue::Number(n) => return n,
+                }
+            }
+        }
+    }
+
+    pub fn evaluate_next(&self) -> Option<ExpressionValue> {
+        if let Some(e) = self.evaluate_next_expression() {
+            Some(e.into())
+        } else if let Some(e) = self.evaluate_next_operation() {
+            Some(e)
+        } else {
+            None
+        }
+    }
+
+    fn evaluate_next_expression(&self) -> Option<Expression> {
+        let mut expr = None;
+        let mut evaluated = false;
+        for (i, val) in self.values.iter().enumerate() {
+            match val {
+                ExpressionValue::Expression(sub_expr) => {
+                    if let Some(sub_expr) = sub_expr.evaluate_next() {
+                        let mut e = self.clone();
+                        std::mem::replace(&mut e.values[i], sub_expr);
+                        expr = Some(e);
+                        evaluated = true;
+                        break;
+                    }
+                }
+                _ => {},
+            }
+        }
+        expr
+    }
+
+    fn evaluate_next_operation(&self) -> Option<ExpressionValue> {
+        let mut next_op: Option<(usize, OperationPriority)> = None;
+        for (i, op) in self.operations.iter().enumerate() {
+            if let Some((next_i, next_priority)) = next_op {
+                if op.priority() > next_priority {
+                    next_op = Some((i, op.priority()));
+                }
+            } else {
+                next_op = Some((i, op.priority()));
+            }
+        }
+
+        if let Some((next_i, _next_priority)) = next_op {
+            let mut e = self.clone();
+            let op = e.operations.remove(next_i);
+            let a = *e.values.remove(next_i).number();
+            let b = *e.values.remove(next_i).number();
+            let val = match op {
+                Operation::Exponent => a.pow(&b),
+                Operation::Division => a / b,
+                Operation::Multiplication => a * b,
+                Operation::Addition => a + b,
+                Operation::Subtraction => a - b,
+            };
+            if e.values.is_empty() {
+                Some(val.into())
+            } else {
+                e.values.insert(next_i, val.into());
+                Some(e.into())
             }
         } else {
-            self.number
+            None
         }
     }
 }
+
+pub type OperationPriority = u8;
 
 #[derive(Debug, Clone)]
-enum Operation {
-    Exponent(Box<Expression>),
-    Division(Box<Expression>),
-    Multiplication(Box<Expression>),
-    Addition(Box<Expression>),
-    Subtraction(Box<Expression>),
+pub enum Operation {
+    Exponent,
+    Division,
+    Multiplication,
+    Addition,
+    Subtraction,
 }
 
-fn evaluate_str(expression: &str) -> Result<RationalNumber, LibError> {
-    if let Some((sub_expr, i)) = find_next_group(expression) {
-        let next_expr = format!("{}{}{}", &expression[0..i], evaluate_str(&sub_expr)?, &expression[i+sub_expr.len()..]);
-        evaluate_str(&next_expr)
-    } else {
-        println!("time for: {}", expression);
-        unimplemented!()
-    }
-}
-
-fn find_next_group(expression: &str) -> Option<(&str, usize)> {
-    let mut start = 0;
-    let mut levels_deep = 0;
-    for (i, c) in expression.chars().into_iter().enumerate() {
-        if c == '(' {
-            if start == 0 {
-                start = i+1;
-            }
-            levels_deep += 1;
-        } else if c == ')' {
-            levels_deep -= 1;
-            if levels_deep == 0 {
-                return Some((&expression[start..i], start));
-            }
+impl Operation {
+    fn priority(&self) -> OperationPriority {
+        match self {
+            Operation::Exponent => 2,
+            Operation::Division => 1,
+            Operation::Multiplication => 1,
+            Operation::Addition => 0,
+            Operation::Subtraction => 0,
         }
     }
-    None
 }
 
 #[cfg(test)]
@@ -127,6 +227,15 @@ mod tests {
         let e = Expression::new(2)
             .add(3)
             .multiply(7);
-        assert_eq!(e.evaluate().as_i32().unwrap(), 35);
+        assert_eq!(e.evaluate().as_i32().unwrap(), 23);
+    }
+
+    #[test]
+    fn evaluates_with_subexpressions() {
+        let sub_a = Expression::new(4).add(1);
+        let sub_b = Expression::new(5).add(8);
+
+        let e = Expression::new(sub_a).subtract(sub_b);
+        assert_eq!(e.evaluate().as_i32().unwrap(), -8);
     }
 }
